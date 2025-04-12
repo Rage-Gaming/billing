8d6a6413a0f91a0291e12bc2342ce407b7570706
Rage Gaming <72497418+Rage-Gaming@users.noreply.github.com>
2025-04-13 01:40:27 +0530

    FEAT: Added colors in console

--- server/config/db.cjs
+++ server/config/db.cjs
@@ -1,12 +1,12 @@
 require('dotenv').config();
+require('colors');
 const mongoose = require('mongoose');
-
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI, {});
-    console.log('MongoDB connected successfully');
+    console.log('✅ MongoDB connected successfully'.green);
   } catch (err) {
-    console.error('MongoDB connection error:', err);
+    console.error('❌ MongoDB connection error:'.red, err);
     process.exit(1);
   }
 };
--- server/server.cjs
+++ server/server.cjs
@@ -1,6 +1,9 @@
+require('dotenv').config();
+require('colors');
 const app = require('./app.cjs');
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.clear();
+  console.log(`✅ Server running on port ${PORT}`.green);
 });
